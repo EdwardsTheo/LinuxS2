c34fb46ee7c177c5e51966cf864d66f1d1403556
BaptisteTheo <48800623+EdwardsTheo@users.noreply.github.com>
2022-06-16 16:22:44 +0200

    Update ScriptServer.sh

--- ScriptServer.sh
+++ ScriptServer.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-while read user
+while read user # Ne s'arrête pas tant qu'il reçois des arguments
 do
-        exists=$(grep -c "^$user:" /etc/passwd) #Check if the user exist
+        exists=$(grep -c "^$user:" /etc/passwd) # Vérifie que l'user existe
         if [ $exists -eq 0 ]; then
-                `useradd -m -s /bin/bash $user` # Add user
-                sh -c 'echo '$user':'$user' | chpasswd' # Change the password
-                echo "the user $user have been added"
+                `useradd -m -s /bin/bash $user` # Ajoute l'user
+                sh -c 'echo '$user':'$user' | chpasswd' # Change le mot de passe pour une connexion en ssh
+                echo "the user $user have been added" # Réponse pour le client
         else
-                echo "The user $user exists"
+                echo "The user $user exists" # Réponse pour le client
 fi
 done
 
